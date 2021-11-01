2e4629cb6ad06cc89f3a08b258495b401f91b655
Rysias <jvhithamar@gmail.com>
2021-11-01 15:59:20 +0100

    add azure_attempt :))

---
+++ wheres-waldo/src/azure_attempt.py
@@ -0,0 +1,24 @@
+from azure.cognitiveservices.vision.customvision.training import (
+    CustomVisionTrainingClient,
+)
+from azure.cognitiveservices.vision.customvision.prediction import (
+    CustomVisionPredictionClient,
+)
+from azure.cognitiveservices.vision.customvision.training.models import (
+    ImageFileCreateBatch,
+    ImageFileCreateEntry,
+    Region,
+)
+from msrest.authentication import ApiKeyCredentials
+import os, time, uuid
+import json
+
+
+def load_json(file_path):
+    with open(file_path, "r") as f:
+        return json.load(f)
+
+
+ENDPOINT = "PASTE_YOUR_CUSTOM_VISION_TRAINING_ENDPOINT_HERE"
+training_key = "PASTE_YOUR_CUSTOM_VISION_TRAINING_SUBSCRIPTION_KEY_HERE"
+

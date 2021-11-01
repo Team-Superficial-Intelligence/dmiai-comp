82586d1150e841e4e25c9b5c2db95cf4f5c41bb1
Rysias <jvhithamar@gmail.com>
2021-11-01 15:59:42 +0100

    start tutorial

--- wheres-waldo/src/azure_attempt.py
+++ wheres-waldo/src/azure_attempt.py
@@ -12,6 +12,7 @@ from azure.cognitiveservices.vision.customvision.training.models import (
 from msrest.authentication import ApiKeyCredentials
 import os, time, uuid
 import json
+from pathlib import Path
 
 
 def load_json(file_path):
@@ -19,6 +20,29 @@ def load_json(file_path):
         return json.load(f)
 
 
-ENDPOINT = "PASTE_YOUR_CUSTOM_VISION_TRAINING_ENDPOINT_HERE"
-training_key = "PASTE_YOUR_CUSTOM_VISION_TRAINING_SUBSCRIPTION_KEY_HERE"
+configs = load_json(Path("../../../azure_keys.json"))
 
+ENDPOINT = configs["endpoint"]
+training_key = configs["train_key"]
+
+# Auth
+credentials = ApiKeyCredentials(in_headers={"Training-key": training_key})
+trainer = CustomVisionTrainingClient(ENDPOINT, credentials)
+
+# Create project
+publish_iteration_name = "waldominator1"
+
+# Find the object detection domain
+obj_detection_domain = next(
+    domain
+    for domain in trainer.get_domains()
+    if domain.type == "ObjectDetection" and domain.name == "General"
+)
+
+# Create a new project
+print("Creating project...")
+# Use uuid to avoid project name collisions.
+project = trainer.create_project(str(uuid.uuid4()), domain_id=obj_detection_domain.id)
+
+# add tags
+waldo_tag = trainer.create_tag(project.id, "waldo")

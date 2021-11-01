3e9fcdbf1c2ee4e65dac5b40065f2f91d5aa46e7
Rysias <jvhithamar@gmail.com>
2021-11-01 16:14:12 +0100

    normalize bboxes

--- wheres-waldo/src/azure_attempt.py
+++ wheres-waldo/src/azure_attempt.py
@@ -12,7 +12,9 @@ from azure.cognitiveservices.vision.customvision.training.models import (
 from msrest.authentication import ApiKeyCredentials
 import os, time, uuid
 import json
+import cv2
 from pathlib import Path
+from typing import List
 
 
 def load_json(file_path):
@@ -20,10 +22,26 @@ def load_json(file_path):
         return json.load(f)
 
 
+def reformat_bbox(bbox: List[int], img_shape) -> List[float]:
+    """ 
+    Inputs a bounding box with coord (xmin, ymin, xmax, ymax)
+    transforms into microsoft format (normalized left, top, width, height)
+    """
+    width = bbox[2] - bbox[0]
+    height = bbox[3] - bbox[1]
+    return [
+        bbox[0] / img_shape[1],
+        bbox[1] / img_shape[0],
+        width / img_shape[1],
+        height / img_shape[0],
+    ]
+
+
 configs = load_json(Path("../../../azure_keys.json"))
 
 ENDPOINT = configs["endpoint"]
 training_key = configs["train_key"]
+TRAINING_DIR = Path("../../example-data/wheres-waldo/training_imgs")
 
 # Auth
 credentials = ApiKeyCredentials(in_headers={"Training-key": training_key})
@@ -46,3 +64,16 @@ project = trainer.create_project(str(uuid.uuid4()), domain_id=obj_detection_doma
 
 # add tags
 waldo_tag = trainer.create_tag(project.id, "waldo")
+
+# Loading data
+bbox_paths = list((TRAINING_DIR / "bboxes").glob("*.json"))
+bboxes = [load_json(bbox_path) for bbox_path in bbox_paths]
+
+img_paths = list(TRAINING_DIR.glob("*.png"))
+img_shape = cv2.imread(str(img_paths[0])).shape
+
+waldo_image_regions = {
+    filename: reformat_bbox(bbox, img_shape)
+    for bbox_dict in bboxes
+    for filename, bbox in bbox_dict.items()
+}

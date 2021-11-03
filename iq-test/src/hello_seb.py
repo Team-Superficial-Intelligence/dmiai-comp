d2c9a6daa0771b26a133393d4c11cabc89451495
Sebastian Scott Engen <snuggle.hogwarts@gmail.com>
2021-11-03 13:40:31 +0100

    Update hello_seb.py

--- iq-test/src/hello_seb.py
+++ iq-test/src/hello_seb.py
@@ -7,3 +7,7 @@ test_img = fii.read_img(img_files[0])
 image_list = fii.split_img(test_img)
 
 fii.show_img(image_list[0][0])
+
+print(image_list[0][0].shape)
+# (225, 400, 3)
+cv2.rotate(image_list[0][0])

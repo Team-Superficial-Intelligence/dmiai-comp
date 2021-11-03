2ff4018e0a8ce6c49e657fcacee023f33a877fb9
Sebastian Scott Engen <snuggle.hogwarts@gmail.com>
2021-11-03 16:54:07 +0100

    Update hello_seb.py

--- iq-test/src/hello_seb.py
+++ iq-test/src/hello_seb.py
@@ -12,8 +12,20 @@ test_img = fii.read_img(img_files[0])
 
 image_list = fii.split_img(test_img)
 
-#fii.show_img(image_list[0][0])
+cv2.imshow("Image",image_list[0][0])
+cv2.destroyAllWindows(5)
+
+# Rotate images 45 degrees
+img_rotate_45_clockwise = cv2.rotate(image_list[0][0], cv2.ROTATE_45_CLOCKWISE)
+##fii.show_img(img_rotate_45_clockwise)
 
 # Rotate images 90 degrees
 img_rotate_90_clockwise = cv2.rotate(image_list[0][0], cv2.ROTATE_90_CLOCKWISE)
-fii.show_img(img_rotate_90_clockwise)
+##fii.show_img(img_rotate_90_clockwise)
+
+# Rotate images 180 degrees
+img_rotate_180_clockwise = cv2.rotate(image_list[0][0], cv2.ROTATE_180_CLOCKWISE)
+##fii.show_img(img_rotate_180_clockwise)
+
+# Game of life
+## Find red 

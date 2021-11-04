e701ac3fa1a70fa0b344c9fe38d174dd9cd4967e
Rysias <jvhithamar@gmail.com>
2021-11-04 19:26:40 +0100

    update checks

--- iq-test/src/full_check.py
+++ iq-test/src/full_check.py
@@ -1,10 +1,10 @@
+import random
+
 import format_iq_imgs as fii
 import color_check as cc
 import rotate_check as rc
 import red_dot_check as rd
 import rounding_check as ro
-
-img_paths = rc.find_img_files()
 
 
 def check_shit(img_path):
@@ -14,20 +14,21 @@ def check_shit(img_path):
     img_list = fii.split_img(img)
 
     rotation_result = rc.check_rotations(img_list, choices)
-    if rotation_result:
+    if rotation_result is not None:
         return rotation_result
 
     bitxor_result = cc.check_xor(img_list, choices)
-    if bitxor_result:
+    if bitxor_result is not None:
         return bitxor_result
 
     bitand_result = cc.check_bitand(img_list, choices)
-    if bitand_result:
+    if bitand_result is not None:
         return bitand_result
 
-    return None
+    return random.choice(range(len(choices)))
 
 
+img_paths = rc.find_img_files()
 for img_path in img_paths:
     img_path = img_paths[0]
     print(check_shit(img_path))

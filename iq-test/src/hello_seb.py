13baa977f723762b027d13026a3efdc6fec67e68
Sebastian Scott Engen <snuggle.hogwarts@gmail.com>
2021-11-04 10:58:41 +0100

    Update hello_seb.py

--- iq-test/src/hello_seb.py
+++ iq-test/src/hello_seb.py
@@ -1,29 +1,62 @@
+# import the necessary packages
 import format_iq_imgs as fii
 import numpy as np
 import cv2 as cv
 from pathlib import Path
 import os
 from PIL import Image
+from imutils import build_montages
+from imutils import paths
+import argparse
+import imutils
 
 os.getcwd()
 IMG_DIR = Path("../../example-data/iq-test/dmi-api-test")
 IMG_DIR.exists()
 
 img_files = list(IMG_DIR.glob("*image*.png"))
-test_img = fii.read_img(img_files[3])
+test_img = fii.read_img(img_files[8])
 image_list = fii.split_img(test_img)
-## Circle
-Circle = np.zeros([110,110,3],dtype=np.uint8)
-Circle.fill(255) # numpy array! 
-Circle= cv.circle(Circle,(55,48),27,(0,0,0),-1)
-#Circle = Image.fromarray(Circle) #convert numpy array to image
 # Image to alter
-TestImg = image_list[2][0]
-TestImg.shape
-## Combine to "soften"
-Combined = cv.bitwise_or(TestImg,Circle)
+image = image_list[0][0]
+image.shape
+# Define Colors
+## define the list of boundaries
+boundaries = [
+	([0, 0, 50], [60, 60, 200])
+]
+## Function for colorfulness
+def image_colorfulness(image):
+    # split the image into its respective RGB components
+	(B, G, R) = cv.split(image.astype("float"))
+	# compute rg = R - G
+	rg = np.absolute(R - G)
+	# compute yb = 0.5 * (R + G) - B
+	yb = np.absolute(0.5 * (R + G) - B)
+	# compute the mean and standard deviation of both `rg` and `yb`
+	(rbMean, rbStd) = (np.mean(rg), np.std(rg))
+	(ybMean, ybStd) = (np.mean(yb), np.std(yb))
+	# combine the mean and standard deviations
+	stdRoot = np.sqrt((rbStd ** 2) + (ybStd ** 2))
+	meanRoot = np.sqrt((rbMean ** 2) + (ybMean ** 2))
+	# derive the "colorfulness" metric and return it
+	return stdRoot + (0.3 * meanRoot)
 
-cv.imshow("Image",Combined)
+# loop over the boundaries
+for (lower, upper) in boundaries:
+	# create NumPy arrays from the boundaries
+	lower = np.array(lower, dtype = "uint8")
+	upper = np.array(upper, dtype = "uint8")
+	# find the colors within the specified boundaries and apply
+	# the mask
+	mask = cv.inRange(image, lower, upper)
+	output = cv.bitwise_and(image, image, mask = mask)
+
+#Print Color Value
+C = image_colorfulness(output)
+C
+#Potentially print
+cv.imshow("Image",output)
 cv.waitKey()
 cv.destroyAllWindows() 
 
@@ -152,5 +185,95 @@ bitwise_addition = cv.bitwise_and(Pic1, Pic2)
 bitwise_addition = cv.cvtColor(bitwise_addition, cv.COLOR_RGBA2GRAY)
 ### Print
 cv.imshow("Bitwise_Addition",bitwise_addition)
+cv.waitKey()
+cv.destroyAllWindows() 
+
+# Soften
+import format_iq_imgs as fii
+import numpy as np
+import cv2 as cv
+from pathlib import Path
+import os
+from PIL import Image
+
+os.getcwd()
+IMG_DIR = Path("../../example-data/iq-test/dmi-api-test")
+IMG_DIR.exists()
+
+img_files = list(IMG_DIR.glob("*image*.png"))
+test_img = fii.read_img(img_files[3])
+image_list = fii.split_img(test_img)
+## Circle
+Circle = np.zeros([110,110,3],dtype=np.uint8)
+Circle.fill(255) # numpy array! 
+Circle= cv.circle(Circle,(55,55),30,(0,0,0),-1)
+#Circle = Image.fromarray(Circle) #convert numpy array to image
+# Image to alter
+TestImg = image_list[3][0]
+TestImg.shape
+## Combine to "soften"
+Combined = cv.bitwise_or(TestImg,Circle)
+
+
+# Color dots
+# import the necessary packages
+import format_iq_imgs as fii
+import numpy as np
+import cv2 as cv
+from pathlib import Path
+import os
+from PIL import Image
+from imutils import build_montages
+from imutils import paths
+import argparse
+import imutils
+
+os.getcwd()
+IMG_DIR = Path("../../example-data/iq-test/dmi-api-test")
+IMG_DIR.exists()
+
+img_files = list(IMG_DIR.glob("*image*.png"))
+test_img = fii.read_img(img_files[8])
+image_list = fii.split_img(test_img)
+# Image to alter
+image = image_list[0][0]
+image.shape
+# Define Colors
+## define the list of boundaries
+boundaries = [
+	([0, 0, 50], [60, 60, 200])
+]
+## Function for colorfulness
+def image_colorfulness(image):
+    # split the image into its respective RGB components
+	(B, G, R) = cv.split(image.astype("float"))
+	# compute rg = R - G
+	rg = np.absolute(R - G)
+	# compute yb = 0.5 * (R + G) - B
+	yb = np.absolute(0.5 * (R + G) - B)
+	# compute the mean and standard deviation of both `rg` and `yb`
+	(rbMean, rbStd) = (np.mean(rg), np.std(rg))
+	(ybMean, ybStd) = (np.mean(yb), np.std(yb))
+	# combine the mean and standard deviations
+	stdRoot = np.sqrt((rbStd ** 2) + (ybStd ** 2))
+	meanRoot = np.sqrt((rbMean ** 2) + (ybMean ** 2))
+	# derive the "colorfulness" metric and return it
+	return stdRoot + (0.3 * meanRoot)
+
+# loop over the boundaries
+for (lower, upper) in boundaries:
+	# create NumPy arrays from the boundaries
+	lower = np.array(lower, dtype = "uint8")
+	upper = np.array(upper, dtype = "uint8")
+	# find the colors within the specified boundaries and apply
+	# the mask
+	mask = cv.inRange(image, lower, upper)
+	output = cv.bitwise_and(image, image, mask = mask)
+
+#Print Color Value
+C = image_colorfulness(output)
+C
+#Potentially print
+cv.imshow("Image",output)
 cv.waitKey()
 cv.destroyAllWindows() 

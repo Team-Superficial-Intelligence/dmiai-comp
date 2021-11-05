7729c39611a4fac47a5e8efaefd8a8bc57218eb2
Rysias <jvhithamar@gmail.com>
2021-11-05 12:12:24 +0100

    add ending

--- iq-test/shit_checker/star_check.py
+++ iq-test/shit_checker/star_check.py
@@ -15,17 +15,18 @@ import imutils
 import matplotlib.pyplot as plt
 
 os.getcwd()
-IMG_DIR = Path("../../example-data/iq-test/dmi-api-test")
+IMG_DIR = Path("../example-data/iq-test/dmi-api-test")
 IMG_DIR.exists()
 
 img_files = list(IMG_DIR.glob("*image*.png"))
-test_img = fii.read_img(img_files[0])
+test_img = fii.read_img(img_files[-3])
 image_list = fii.split_img(test_img)
 # Image to alter
 image = image_list[0][0]
+fii.show_img(image)
 
 # Convert BGR to HSV
-#hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+# hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 # https://answers.opencv.org/question/229620/drawing-a-rectangle-around-the-red-color-region/
 
 # red color boundaries [B, G, R]
@@ -38,7 +39,7 @@ upper = np.array(upper, dtype="uint8")
 mask = cv.inRange(image, lower, upper)
 output = cv.bitwise_and(image, image, mask=mask)
 
-kernel = np.ones((5,5),np.uint8)
+kernel = np.ones((5, 5), np.uint8)
 output = cv.dilate(output, kernel, iterations=3)
 
 # Rotate Triangle
@@ -49,5 +50,4 @@ output = cv.dilate(output, kernel, iterations=3)
 # rotate our output image by 144 degrees around the center of the image
 M = cv.getRotationMatrix2D((cX, cY), 144, 1.0)
 rotated = cv.warpAffine(output, M, (w, h))
-cv.imshow("Rotated by 144 Degrees", rotated)
-cv.waitKey(0)
+fii.show_img(rotated)

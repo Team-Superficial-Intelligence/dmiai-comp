0fbaeae67370994892118d4ec67c25eef00afa51
Rysias <jvhithamar@gmail.com>
2021-11-05 11:22:30 +0100

    add test stuff

--- iq-test/shit_checker/full_check.py
+++ iq-test/shit_checker/full_check.py
@@ -3,6 +3,7 @@ import random
 import numpy as np
 import cv2
 from typing import List
+from pathlib import Path
 
 import shit_checker.format_iq_imgs as fii
 import shit_checker.color_check as cc
@@ -13,8 +14,12 @@ import shit_checker.no_change_check as ncc
 # import rounding_check as ro
 
 
-def base64_to_cv2(img_string: str) -> np.array:
+def read_img_string(img_path: Path):
+    with open(img_path, "rb") as img:
+        return base64.b64encode(img.read())
 
+
+def base64_to_cv2(img_string: str) -> np.array:
     nparr = np.fromstring(base64.b64decode(img_string), np.uint8)
     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
     return img
@@ -47,6 +52,11 @@ def check_shit(img_string: str, choice_list: List[str]):
     return random.choice(range(len(choices)))
 
 
-# img_paths = rc.find_img_files()
-# for img_path in img_paths:
-#    print(check_shit(img_path))
+if __name__ == "__main__":
+    img_dir = Path("../example-data/iq-test/dmi-api-test")
+    img_paths = rc.find_img_files(img_path=img_dir)
+    for img_path in img_paths:
+        img = read_img_string(img_path)
+        choice_paths = rc.find_img_choices(img_path, img_dir=img_dir)
+        choices = [read_img_string(img_file) for img_file in choice_paths]
+        print(check_shit(img, choices))
